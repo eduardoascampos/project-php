4d78e35cb01314aaa274c579d287dec3f54256f2
eduardoascampos <eduecampos@gmail.com>
2020-06-17 12:58:10 -0300

    Reorganização de layout, inclusão de novas páginas.

--- routes.php
+++ routes.php
@@ -10,21 +10,34 @@ $template = new Tpl;
 $content = array(
 
     "1" => "/index",
-    "2" => "/hello"
-
+    "2" => "/about",
+    "3" => "/services",
+    "4" => "/contact",
 );
+
+
 
 Flight::route('/', function () use($template, $content) {
     $template->draw("templates/header");
     $template->draw("pages{$content['1']}");
     $template->draw("templates/footer");
-
 });
 
-
-Flight::route('/hello', function () use($template, $content){
+Flight::route('/sobre', function () use($template, $content){
     $template->draw("templates/header");
     $template->draw("pages{$content['2']}");
+    $template->draw("templates/footer");
+});
+
+Flight::route('/servicos', function () use($template, $content){
+    $template->draw("templates/header");
+    $template->draw("pages{$content['3']}");
+    $template->draw("templates/footer");
+});
+
+Flight::route('/contato', function () use($template, $content){
+    $template->draw("templates/header");
+    $template->draw("pages{$content['4']}");
     $template->draw("templates/footer");
 });
 
